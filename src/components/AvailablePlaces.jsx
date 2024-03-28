12b07d23ac82ea00c0a9348913353a3daf2c1c74
Sergio <norgestream@gmail.com>
2024-03-28 22:22:08 +0200

    use async await and error handling

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,25 +1,47 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import ErrorMessage from './ErrorMessage.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    fetch('http://localhost:5000/places')
-      .then(response => {
-        return response.json();
-      })
-      .then(resData => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const responce = await fetch('http://localhost:5000/plces');
+        const resData = await responce.json();
+
+        if (!responce.ok) {
+          throw new Error('something went wrong');
+        }
+
         setAvailablePlaces(resData.places);
-      });
+      } catch(error) {
+        setError(error);
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
   }, []);
+
+  if (error) {
+    return <ErrorMessage title='An error occurred' message={error.message} />
+  }
+  
 
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
       fallbackText="No places available."
+      loadingText='Fetching places...'
       onSelectPlace={onSelectPlace}
     />
   );
